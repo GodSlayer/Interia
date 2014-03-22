31f801c868f923a51e77d339afa4deeefa228419
GodSlayer <realgodslayer@gmail.com>
2014-03-21 22:23:14 -0500

    Update Fonts.java

--- interia/font/Fonts.java
+++ interia/font/Fonts.java
@@ -6,10 +6,12 @@ public class Fonts
 {
 	public static UnicodeFontRenderer avenir16;
 	public static UnicodeFontRenderer avenir22;
+	public static UnicodeFontRenderer avenir14;
 	
 	public static void loadFonts()
 	{
 		avenir16 = new UnicodeFontRenderer(new Font("Avenir", Font.PLAIN, 16));
+		avenir14 = new UnicodeFontRenderer(new Font("Lucida Console", Font.PLAIN, 14));
 		avenir22 = new UnicodeFontRenderer(new Font("Avenir", Font.BOLD, 22));
 	}
 }
