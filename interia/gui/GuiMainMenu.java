696d218d4e237639fa6d1ca58a9cb2946390f1ac
GodSlayer <realgodslayer@gmail.com>
2014-03-22 21:59:59 -0500

    Create GuiMainMenu.java

---
+++ interia/gui/GuiMainMenu.java
@@ -0,0 +1,7 @@
+package interia.interia.gui;
+
+import net.minecraft.client.gui.GuiScreen;
+
+public class GuiMainMenu extends GuiScreen{
+    //We Will Code this together later.... call me on skype when you get the chance....
+}
