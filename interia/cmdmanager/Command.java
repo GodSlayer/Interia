a87f7b326acc31465590236141e7646c45969e29
GodSlayer <realgodslayer@gmail.com>
2014-03-22 22:08:24 -0500

    Create Command.java

---
+++ interia/cmdmanager/Command.java
@@ -0,0 +1,17 @@
+package interia.interia.cmdmanager;
+
+public abstract class Command {
+	private String command;
+	
+	public Command(String command) {
+		this.command = command;
+	}
+
+	public abstract void runCommand(String s, String[] args);
+	public abstract String getDescription();
+	public abstract String getSyntax();
+	
+	public String getCommand() {
+		return command;
+	}
+}
